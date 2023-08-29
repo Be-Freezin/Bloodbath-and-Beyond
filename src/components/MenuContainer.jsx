18e82e488823b59341984076916dee50534ea106
Be-Freezin <brayf@live.com>
2023-08-29 17:32:38 -0500

    assets added, MVP nav bar

---
+++ src/components/MenuContainer.jsx
@@ -0,0 +1,12 @@
+import Nav from "./Nav"
+
+const MenuContainer = () => {
+  return (
+		<div className='bg-primary-background rounded-3xl border-8 border-primary-black  mx-0 my-0'>
+      <Nav />
+			
+		</div>
+	)
+}
+
+export default MenuContainer
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'primary-white': 'var(--primary-white)',
+				'header-text': 'var(--primary-white)',
+				'primary-background': 'var(--primary-color)',
+				'primary-text': 'var(--primary-black)',
+				'primary-button': 'var(--primary-color)',
+				'primary-black': 'var(--primary-black)',
+				'button-muted': 'var(--primary-color-muted)',
+			},
+			fontFamily:{
+				'heroking': ['heroking', 'san-serif'],
+				'inika': ['inika', 'serif']
+
+			}
+		},
 	},
 	plugins: [],
 }
