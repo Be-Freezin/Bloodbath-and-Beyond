058d1b262d8497e1cc8c175be10e397555d66efa
Be-Freezin <brayf@live.com>
2023-09-23 13:51:45 -0500

    put item images into src direct

--- src/ItemData.js
+++ src/ItemData.js
@@ -1,5 +1,5 @@
 export default {
-	'items': [
+	items: [
 		{
 			id: 1,
 			name: 'Soulkeeper',
@@ -7,7 +7,7 @@ export default {
 			price: 500,
 			description:
 				"This elegantly crafted blade is rumored to capture the essence of its foes. Each successful strike has a 15% chance to drain and restore 10% of the wielder's health.",
-			imagePath: 'public/items/Soulkeeper.png',
+			imagePath: 'src/assets/items/Soulkeeper.png',
 		},
 		{
 			id: 2,
@@ -16,7 +16,7 @@ export default {
 			price: 400,
 			description:
 				'Forged from the heart of mountains, this shield not only offers staunch defense but also has a 25% chance to reflect magic attacks back to its caster.',
-			imagePath: 'public/items/Earthward.png',
+			imagePath: 'src/assets/items/Earthward.png',
 		},
 		{
 			id: 3,
@@ -25,7 +25,7 @@ export default {
 			price: 350,
 			description:
 				'Inscribed with runes of the North, this ring grants the user control over ice magic, increasing ice spell potency by 20% and giving a 10% chance to freeze foes on touch.',
-			imagePath: 'public/items/Frostweaver Ring.png',
+			imagePath: 'src/assets/items/Frostweaver Ring.png',
 		},
 		{
 			id: 4,
@@ -34,7 +34,7 @@ export default {
 			price: 650,
 			description:
 				'Ancient writings adorn this ornate chest, hinting at treasures untold. Opening it guarantees a rare item, with a 5% chance of an epic artifact.',
-			imagePath: 'public/items/Mystery Chest.png',
+			imagePath: 'src/assets/items/Mystery Chest.png',
 		},
 		{
 			id: 5,
@@ -43,7 +43,7 @@ export default {
 			price: 520,
 			description:
 				"Glimmering in twilight hues, this blade cuts through illusions, revealing hidden traps and granting a 20% chance to see an enemy's next move.",
-			imagePath: 'public/items/Veilpiercer.png',
+			imagePath: 'src/assets/items/Veilpiercer.png',
 		},
 		{
 			id: 6,
@@ -52,7 +52,7 @@ export default {
 			price: 530,
 			description:
 				'Crafted during an eclipse, this sword is imbued with shadows. Strikes have a 20% chance to put foes to sleep for 1 turn.',
-			imagePath: 'public/items/Duskmourn.png',
+			imagePath: 'src/assets/items/Duskmourn.png',
 		},
 		{
 			id: 7,
@@ -61,7 +61,7 @@ export default {
 			price: 360,
 			description:
 				"Burning with a sun's fury, wearers of this ring have their fire spells enhanced by 20% and gain a 10% chance to ignite enemies upon physical contact.",
-			imagePath: 'public/items/Sunfire Signet.png',
+			imagePath: 'src/assets/items/Sunfire Signet.png',
 		},
 		{
 			id: 8,
@@ -70,7 +70,7 @@ export default {
 			price: 200,
 			description:
 				'A fiery concoction granting temporary draconic abilities. Drinking it grants the user enhanced strength, increasing damage dealt by 30% for 3 turns.',
-			imagePath: 'public/items/Dragons Blood.png',
+			imagePath: 'src/assets/items/Dragons Blood.png',
 		},
 		{
 			id: 9,
@@ -79,7 +79,7 @@ export default {
 			price: 580,
 			description:
 				'Infused with storm essences, each swing of this hammer conjures thunderous power, giving a 20% chance to stun enemies for 1 turn.',
-			imagePath: 'public/items/Thundercaller.png',
+			imagePath: 'src/assets/items/Thundercaller.png',
 		},
 		{
 			id: 10,
@@ -88,7 +88,7 @@ export default {
 			price: 50,
 			description:
 				'A pristine scroll ready to hold any spell or message. Can be used to record and reproduce a single spell or ability.',
-			imagePath: 'public/items/Blank Scroll.png',
+			imagePath: 'src/assets/items/Blank Scroll.png',
 		},
 		{
 			id: 11,
@@ -97,7 +97,7 @@ export default {
 			price: 590,
 			description:
 				'Carrying the wrath of the tempest, strikes unleash gusts of wind that push enemies back and have a 15% chance to cause whirlwinds, immobilizing foes for 1 turn.',
-			imagePath: 'public/items/Tempest Maul.png',
+			imagePath: 'src/assets/items/Tempest Maul.png',
 		},
 		{
 			id: 12,
@@ -106,7 +106,7 @@ export default {
 			price: 480,
 			description:
 				'Envenomed with serpent essence, stabs inflict poison, dealing damage over time and have a 20% chance to weaken enemy defenses for 2 turns.',
-			imagePath: 'public/items/Viperfang.png',
+			imagePath: 'src/assets/items/Viperfang.png',
 		},
 		{
 			id: 13,
@@ -115,7 +115,7 @@ export default {
 			price: 210,
 			description:
 				"A potion from the ocean's darkest trenches, granting temporary invisibility and a 20% chance to evade any attack for 2 turns.",
-			imagePath: 'public/items/Abyssal Brew.png',
+			imagePath: 'src/assets/items/Abyssal Brew.png',
 		},
 		{
 			id: 14,
@@ -124,7 +124,7 @@ export default {
 			price: 100,
 			description:
 				'A mystical mushroom that lets you hear whispered secrets of the forest. Consuming it reveals hidden doors and traps for 5 turns.',
-			imagePath: 'public/items/Whisperspore.png',
+			imagePath: 'src/assets/items/Whisperspore.png',
 		},
 		{
 			id: 15,
@@ -133,7 +133,7 @@ export default {
 			price: 150,
 			description:
 				"This stylish hat, made from ancient groves, amplifies the user's connection to nature, enhancing druidic spells by 15% and providing a 10% chance to summon a forest creature as an ally for 1 turn.",
-			imagePath: 'public/items/Eldertree Top Hat.png',
+			imagePath: 'src/assets/items/Eldertree Top Hat.png',
 		},
 		{
 			id: 16,
@@ -142,7 +142,7 @@ export default {
 			price: 550,
 			description:
 				'Carved from a block of eternal ice and edged with enchanted steel, this axe has a 25% chance to encase the enemy in ice, reducing their movement speed and accuracy for 2 turns.',
-			imagePath: 'public/items/Frostbite.png',
+			imagePath: 'src/assets/items/Frostbite.png',
 		},
 	],
 }
